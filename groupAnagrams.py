eb38b8d8d817a219d66be39b4848fe542684d569
priyanshu-kr <k2priyanshu@gmail.com>
2026-09-02 10:51:23 +0530

    added groupAnagrams.py

---
+++ groupAnagrams.py
@@ -0,0 +1,31 @@
+from typing import List
+from string import ascii_lowercase
+
+strs = ["cat", "act", "pots", "pit", "tops", "stop"]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        groups = {}
+        
+        for each_str in strs:
+            freq = {}
+            for each_letter in ascii_lowercase:
+                freq[each_letter] = 0
+            
+            for char in each_str:
+                freq[char] += 1
+
+            signature = tuple(freq.values())
+
+            if signature in groups:
+                groups[signature].append(each_str)
+            else:
+                groups[signature] = [each_str]
+        
+        
+        return list(groups.values())
+
+
+sol = Solution()
+print(sol.groupAnagrams(strs))
