e88e63009e75473d8278c515150ae28085ebb47e
priyanshu-kr <k2priyanshu@gmail.com>
2026-09-12 00:05:01 +0530

    longestCommonPrefix.py

---
+++ longestCommonPrefix.py
@@ -0,0 +1,20 @@
+from typing import List
+
+strs = [""]
+
+class Solution:
+    def longestCommonPrefix(self, strs:List[str]) -> str:
+
+        short = min(len(s) for s in strs)
+        result = ""
+
+        for i in range(0, short):
+            char = strs[0][i]
+            for s in strs[1:]:
+                if s[i] != char:
+                    return result 
+            result += char
+        return result
+
+sol = Solution()
+print(sol.longestCommonPrefix(strs))
