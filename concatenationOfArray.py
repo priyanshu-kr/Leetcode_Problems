a79fdee8edab3488056b9dfdc8524fa4029901c5
priyanshu-kr <k2priyanshu@gmail.com>
2026-09-14 11:36:31 +0530

    added "Leetcode1929"

---
+++ concatenationOfArray.py
@@ -0,0 +1,36 @@
+"""
+1929. Concatenation of Array
+
+You are given an integer array nums of length n. Create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+Example 1:
+
+Input: nums = [1,4,1,2]
+
+Output: [1,4,1,2,1,4,1,2]
+Example 2:
+
+Input: nums = [22,21,20,1]
+
+Output: [22,21,20,1,22,21,20,1]
+Constraints:
+
+1 <= nums.length <= 1000.
+1 <= nums[i] <= 1000
+"""
+
+from typing import List
+
+nums = [1,2,3,4]
+
+class Solution:
+    def getConcatenation(self, nums:List[int]) -> List[int]:
+
+        return nums+nums
+
+sol = Solution()
+print(sol.getConcatenation(nums))
