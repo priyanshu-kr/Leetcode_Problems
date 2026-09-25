a279e0b9e7cbbba5ee0de4d62e4204021e634307
priyanshu-kr <k2priyanshu@gmail.com>
2026-09-25 21:47:07 +0530

    added Leetcode15

---
+++ 3Sum.py
@@ -0,0 +1,76 @@
+'''
+15. 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+3 <= nums.length <= 3000
+-10^5 <= nums[i] <= 10^5
+'''
+
+from typing import List
+
+nums = [0,0,0]
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        answer = []
+
+        for i in range(len(nums)):
+            if i > 0  and nums[i] == nums[i-1]:
+                continue
+
+            if nums[i] > 0:
+                break
+
+            L = i + 1
+            R = len(nums) - 1
+
+            target = -nums[i]
+
+            while L < R:
+                current_sum = nums[L] + nums[R]
+
+                if current_sum > target:
+                    R -= 1
+                elif current_sum < target:
+                    L += 1
+                else:
+                    answer.append([nums[i], nums[L], nums[R]])
+
+                    L += 1
+                    R -= 1
+
+                    while L < R and nums[L] == nums[L-1]:
+                        L += 1
+                    while L < R and nums[R] == nums[R+1]:
+                        R -= 1
+
+        return answer
+
+sol = Solution()
+print(sol.threeSum(nums))
